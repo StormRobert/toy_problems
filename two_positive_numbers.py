35690f00b927882aa1fa6d3949623eccf53b35fd
StormRobert <stormarasa2@gmail.com>
2023-11-28 00:53:44 +0300

    took a while but the positive functions perfectly

---
+++ two_positive_numbers.py
@@ -0,0 +1,20 @@
+def are_exactly_two_positive_numbers(a, b, c):
+    positive_count = 0
+
+    if a > 0:
+        positive_count += 1
+
+    if b > 0:
+        positive_count += 1
+
+    if c > 0:
+        positive_count += 1
+
+    result = positive_count == 2
+    print(f"Numbers: {a}, {b}, {c} | Condition: {result}")
+    return result
+
+print(are_exactly_two_positive_numbers(-4, 6, 8)) 
+print(are_exactly_two_positive_numbers(4, -6, 9))  
+print(are_exactly_two_positive_numbers(-4, 6, 0))  
+print(are_exactly_two_positive_numbers(4, 6, 10)) 
